9f5253f88a03612ea7bb751c46b0f57832fcd1cb
Raúl Corchero <corchero100@gmail.com>
2024-05-03 12:05:34 +0100

    Clean usings

--- coffee-machine-d/CoffeeMachineApp/infrastructure/pricecatalog/WebPricesCatalog.cs
+++ coffee-machine-d/CoffeeMachineApp/infrastructure/pricecatalog/WebPricesCatalog.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CoffeeMachineApp.core;
+﻿using CoffeeMachineApp.core;
 
 namespace CoffeeMachineApp.infrastructure.pricecatalog {
     public class WebPricesCatalog : PricesCatalog {
