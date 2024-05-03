a1fff20a874374ef009bbac0d0c481b5120c293b
Raúl Corchero <corchero100@gmail.com>
2024-05-03 12:04:31 +0100

    Creeate WebPricesCatalog

---
+++ coffee-machine-d/CoffeeMachineApp/infrastructure/pricecatalog/WebPricesCatalog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoffeeMachineApp.core;
+
+namespace CoffeeMachineApp.infrastructure.pricecatalog {
+    public class WebPricesCatalog : PricesCatalog {
+        public decimal GetPrice(DrinkType drinkType)
+        {
+            //imagine code
+            return 0;
+        }
+    }
+}
