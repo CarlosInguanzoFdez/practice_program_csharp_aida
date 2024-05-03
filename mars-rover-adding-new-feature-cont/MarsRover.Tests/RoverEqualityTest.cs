24328ea14e62c769beeebdda11089e8ff50458cf
Carlos <carlosinguanzo.ci@gmail.com>
2024-05-03 14:06:29 +0200

    test: ✅ RoverEqualityTest

--- mars-rover-adding-new-feature-cont/MarsRover.Tests/RoverEqualityTest.cs
+++ mars-rover-adding-new-feature-cont/MarsRover.Tests/RoverEqualityTest.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using static MarsRover.Tests.RoverBuilder;
 
 namespace MarsRover.Tests;
 
@@ -7,14 +8,14 @@ public class RoverEqualityTest
     [Test]
     public void Equal_Rovers()
     {
-        Assert.That(new Rover(1, 1, "N"), Is.EqualTo(new Rover(1, 1, "N")));
+        Assert.That(ANasaRover().Build(), Is.EqualTo(ANasaRover().Build()));
     }
 
     [Test]
     public void Not_Equal_Rovers()
     {
-        Assert.That(new Rover(1, 1, "N"), Is.Not.EqualTo(new Rover(1, 1, "S")));
-        Assert.That(new Rover(1, 1, "N"), Is.Not.EqualTo(new Rover(1, 2, "N")));
-        Assert.That(new Rover(1, 1, "N"), Is.Not.EqualTo(new Rover(0, 1, "N")));
+        Assert.That(ANasaRover().Facing("N").Build(), Is.Not.EqualTo(ANasaRover().Facing("S").Build()));
+        Assert.That(ANasaRover().WithY(10).Build(), Is.Not.EqualTo(ANasaRover().WithY(2).Build()));
+        Assert.That(ANasaRover().WithX(8).Build(), Is.Not.EqualTo(ANasaRover().WithX(7).Build()));
     }
 }
