9c55b72d365cf6e4fb459aa7c42b92f2618473ae
Raúl Corchero <corchero100@gmail.com>
2024-05-08 15:23:52 +0100

    Refactor: move test to abstract class

--- mars-rover-adding-new-feature-cont-3/MarsRover.Tests/RoverUsingCnsaCommunicationProtocolTest.cs
+++ mars-rover-adding-new-feature-cont-3/MarsRover.Tests/RoverUsingCnsaCommunicationProtocolTest.cs
@@ -7,56 +7,6 @@ namespace MarsRover.Tests;
 public class RoverUsingCnsaCommunicationProtocolTest : RoverUsingCommunicationProtocolTest
 {
     [Test]
-    public void No_Commands()
-    {
-        var rover = GetRoverBuilder().Build();
-
-        rover.Receive("");
-
-        Assert.That(rover, Is.EqualTo(CsnaRover().Build()));
-    }
-
-    [Test]
-    public void Forward_Commands()
-    {
-        var rover = CsnaRover().WithCoordinates(0, 0).Facing("N").Build();
-
-        rover.Receive(GetForwardCommandRepresentation());
-
-        Assert.That(rover, Is.EqualTo(GetRoverBuilder().WithCoordinates(0, 1).Facing("N").Build()));
-    }
-
-    [Test]
-    public void Backward_Commands()
-    {
-        var rover = GetRoverBuilder().WithCoordinates(3, 3).Facing("E").Build();
-
-        rover.Receive(GetBackwardCommandRepresentation());
-
-        Assert.That(rover, Is.EqualTo(GetRoverBuilder().WithCoordinates(2, 3).Facing("E").Build()));
-    }
-
-    [Test]
-    public void Rotate_Left_Commands()
-    {
-        var rover = GetRoverBuilder().Facing("E").Build();
-
-        rover.Receive(GetRotateLeftCommandRepresentation());
-
-        Assert.That(rover, Is.EqualTo(GetRoverBuilder().Facing("N").Build()));
-    }
-
-    [Test]
-    public void Rotate_Right_Commands()
-    {
-        var rover = GetRoverBuilder().Facing("W").Build();
-
-        rover.Receive(GetRotateRightCommandRepresentation());
-
-        Assert.That(rover, Is.EqualTo(GetRoverBuilder().Facing("N").Build()));
-    }
-
-    [Test]
     public void Two_Commands()
     {
         var rover = GetRoverBuilder().Facing("W").Build();
