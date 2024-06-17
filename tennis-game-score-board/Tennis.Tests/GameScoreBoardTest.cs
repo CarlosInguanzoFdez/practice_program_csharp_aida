8e7fa712e3748c3349065206c8ee465e6177d1c7
Carlos <carlosinguanzo.ci@gmail.com>
2024-06-17 16:12:47 +0200

    Refactor: ♻️ create SetUp in Test

--- tennis-game-score-board/Tennis.Tests/GameScoreBoardTest.cs
+++ tennis-game-score-board/Tennis.Tests/GameScoreBoardTest.cs
@@ -10,16 +10,32 @@ public class GameScoreBoardTest
     private Reader _reader;
     private GameScoreBoard _gameScoreBoard;
 
-    [Test]
-    public void score_board_is_fifteen_love_when_start_match_and_player1_has_scored()
+    [SetUp]
+    public void SetUp()
     {
         _reader = Substitute.For<Reader>();
         _notifier = Substitute.For<Notifier>();
         _gameScoreBoard = new GameScoreBoard(_reader, _notifier);
+    }
+
+    [Test]
+    public void score_board_is_fifteen_love_when_start_match_and_player1_has_scored()
+    {
         _reader.Read().Returns("$ score 1");
 
         _gameScoreBoard.StartGame();
 
         _notifier.Received(1).Notify("Fifteen Love");
+    }
+
+    [Ignore("")]
+    [Test]
+    public void score_board_is_love_fifteen_when_start_match_and_player2_has_scored()
+    {
+        _reader.Read().Returns("$ score 2");
+
+        _gameScoreBoard.StartGame();
+
+        _notifier.Received(1).Notify("Love Fifteen");
     }
 }
